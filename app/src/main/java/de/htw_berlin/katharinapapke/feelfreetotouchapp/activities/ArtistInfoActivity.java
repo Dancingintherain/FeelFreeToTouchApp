0eada861288e714a10a468f933962d380af66072
s0549129 <s0549129@htw-berlin.de>
2017-05-30 12:59:55 +0200

    scrollable textView ArtistInfoActivity

--- app/src/main/java/de/htw_berlin/katharinapapke/feelfreetotouchapp/activities/ArtistInfoActivity.java
+++ app/src/main/java/de/htw_berlin/katharinapapke/feelfreetotouchapp/activities/ArtistInfoActivity.java
@@ -6,19 +6,25 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import de.htw_berlin.katharinapapke.feelfreetotouchapp.R;
 
 public class ArtistInfoActivity extends AppCompatActivity {
+
+    private TextView aboutArtistText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.menutoolbar);
+        aboutArtistText = (TextView) findViewById(R.id.textAboutArtist);
+        aboutArtistText.setMovementMethod(new ScrollingMovementMethod());
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
